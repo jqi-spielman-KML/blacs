6c76fbcec4c05fecb3d2b977e43861ff3dc807bd
Minilab <rubidium@nist.gov>
2025-05-30 17:58:40 -0400

    Lyse Host issues
    Test commit to fix Lyse Host issues

--- blacs/__version__.py
+++ blacs/__version__.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 try:
     import importlib.metadata as importlib_metadata
@@ -6,20 +7,12 @@ except ImportError:
 
 root = Path(__file__).parent.parent
 if (root / '.git').is_dir():
-    try:
-        from setuptools_scm import get_version
-        VERSION_SCHEME = {
-            "version_scheme": "release-branch-semver",
-            "local_scheme": "node-and-date",
-        }
-        scm_version = get_version(root, **VERSION_SCHEME)
-    except ImportError:
-        scm_version = None
-else:
-    scm_version = None
-
-if scm_version is not None:
-    __version__ = scm_version
+    from setuptools_scm import get_version
+    __version__ = get_version(
+        root,
+        version_scheme="release-branch-semver",
+        local_scheme=os.getenv("SCM_LOCAL_SCHEME", "node-and-date"),
+    )
 else:
     try:
         __version__ = importlib_metadata.version(__package__)
